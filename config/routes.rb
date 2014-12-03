85a3148fa5a6f11ca4619c88ee5760d669e6ba06
Adam Davies <adzdavies@gmail.com>
2014-12-03 12:00:02 +1030

    Whitespace/indentation of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.feefo_fetch_reviews       '/feefo/fetch_reviews/:product_code',   controller: :feefo,             action: 'fetch_reviews', conditions: {method: :get}
+  map.feefo_fetch_reviews '/feefo/fetch_reviews/:product_code',
+                          controller: :feefo, 
+                          action:     'fetch_reviews',
+                          conditions: {method: :get}
 
 end
